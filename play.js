f03d394b30b4edd765d13dd66d839077ed1dbbaa
Adib Zefar <adibzefar@gmail.com>
2022-04-22 18:30:06 -0400

    Add client.js and update play.js

--- play.js
+++ play.js
@@ -1,27 +1,5 @@
-const net = require("net");
-
-// establishes a connection with the game server
-const connect = function () {
-  const conn = net.createConnection({
-    host: "localhost", // IP address here,
-    port: 50541 // PORT number here,
-  });
-
-  conn.on('connect', () => {
-    // code that does something when the connection is first established
-    // console.log('Server says: ', "you ded cuz you idled")
-    console.log('You have connected');
-  });
-
-  conn.on('data', (data) => {
-    console.log('Server says: ', data);
-  });
-
-  // interpret incoming data as text
-  conn.setEncoding("utf8");
-
-  return conn;
-};
+// const net = require("net");
+const connect = require('./client')
 
 console.log("Connecting ...");
 connect();
